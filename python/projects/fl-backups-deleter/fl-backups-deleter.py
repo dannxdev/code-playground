f547734099e9cebe3dfac6a65b30276fce9b6ce2
dannxdev <210926315+dannxdev@users.noreply.github.com>
2026-01-08 10:42:51 -0500

    Modified: Se mejoro la funcionalidad del script.
    path: python/projects/fl-backups-deleter/fl-backups-deleter.py

--- python/projects/fl-backups-deleter/fl-backups-deleter.py
+++ python/projects/fl-backups-deleter/fl-backups-deleter.py
@@ -2,8 +2,6 @@ from pathlib import Path
 import os
 import shutil
 
-os.system('cls')
-
 
 def is_empty_dir(path_dir: Path):
     """Verifica si la carpeta es vacia (True) o no (False)"""
@@ -26,29 +24,53 @@ def delete_content_dir(path_dir):
         print(f"La carpeta '{path_dir}' no existe.")
 
 
+def show_content_dir(path_dir: Path):
+    """Muestra todo el contenido de una carpeta."""
+
+    if path_dir.is_dir() and not is_empty_dir(path_dir):
+
+        print(f"Analizando el contenido de: \n\n{path_dir}\n")
+
+        for item in path_dir.iterdir():
+            print(f"- {item.name}")
+
+    else:
+        print("La ruta especificada no contiene ningún elemento.")
+
+
 def backups_scan(path_dir: Path):
     """Escanea un directorio y su contenido en busca
     de carpetas backup"""
 
     for element in path_dir.iterdir():
         if element.is_dir() and not is_empty_dir(element):
+            # Si el item es una carpeta y no esta vacia.
 
             if element.name == 'Backup':
-                print(f"Se eliminara el contenido de: {element}")
+                # Si la carpeta tiene el nombre Backup.
+                print("==========================")
+                print("Carpeta Backup encontrada.\n")
+                # Mostrando el contenido de la carpeta.
+                show_content_dir(element)
+                print(f"\nSe eliminara el contenido de: {element}")
+
                 while True:
+                    # Bucle de confirmation. Para seguridad.
                     user_check = input(
-                        "¿Confirmar eliminacion? (s/n): ").lower()
+                        "\n¿Confirmar elimination? (s/n): ").lower()
                     if user_check in ('s', 'n'):
                         break
                     print("Opción no valida. Vuelva a intentarlo.")
 
                 if user_check == 's':
+                    # Si el usuario confirma la elimination.
                     delete_content_dir(element)
 
                 if user_check == 'n':
                     print("Operación cancelada.")
 
             else:
+                # Si la carpeta tiene otro nombre.
                 backups_scan(element)
 
 
@@ -59,7 +81,7 @@ if __name__ == "__main__":
 
     while True:
         print(f"\nSe encuentra en el directorio:\n{CURRENT_PATH}")
-        user_option = input("\n¿Desea continuar? (s/n): ")
+        user_option = input("\n¿Desea continuar? (s/n): ").lower()
         if user_option in ('s', 'n'):
             break
         print("Opción no valida. Vuelva a intentarlo.")
