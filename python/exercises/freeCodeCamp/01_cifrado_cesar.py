ec206ab523d1e5745f5ea8c84784ef5549a5fba3
dannxdev <210926315+dannxdev@users.noreply.github.com>
2026-01-13 15:21:44 -0500

    Modified: Se añadieron comentarios al código.
    Details:
    - modified: python/exercises/freeCodeCamp/01_cifrado_cesar.py

--- python/exercises/freeCodeCamp/01_cifrado_cesar.py
+++ python/exercises/freeCodeCamp/01_cifrado_cesar.py
@@ -1,19 +1,25 @@
 def caesar(text, shift, encrypt=True):
-
+    # Verifica si shift es un entero
     if not isinstance(shift, int):
         return 'Shift must be an integer value.'
 
+    # Verifica si shift está entre 1 y 25
     if shift < 1 or shift > 25:
         return 'Shift must be an integer between 1 and 25.'
 
+    # Define el alfabeto base en minúsculas
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+    # Invierte el shift para desencriptar si encrypt es False
     if not encrypt:
         shift = - shift
 
+    # Crea el alfabeto desplazado rotando el original
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    # Genera la tabla de traducción para minúsculas y mayúsculas
     translation_table = str.maketrans(
         alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    # Aplica la traducción al texto
     encrypted_text = text.translate(translation_table)
     return encrypted_text
 
